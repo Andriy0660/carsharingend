d3d02fa19e68979a5aa2fb1c1350d706dfc3095c
Andriy <sandriy666@gmail.com>
2023-05-16 20:21:26 +0300

    fregf

--- src/main/java/com/example/carsharing/CarsharingApplication.java
+++ src/main/java/com/example/carsharing/CarsharingApplication.java
@@ -3,11 +3,19 @@ package com.example.carsharing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
-
+@RestController
+@RequestMapping("/nothing")
 public class CarsharingApplication {
+    @GetMapping("/nothing")
+    public String nth(){
+        return "nothing";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CarsharingApplication.class, args);
