9a1e3d81d9aea27629ab20725b5b528c78e220be
Norbert <d4zershow@gmail.com>
2023-05-25 02:15:30 +0300

    Authentication

--- src/main/java/com/example/carsharing/dto/request/RegisterRequest.java
+++ src/main/java/com/example/carsharing/dto/request/RegisterRequest.java
@@ -13,16 +13,14 @@ public class RegisterRequest {
     @Email(message = "Invalid email address")
     @NotBlank(message = "Email must be not null")
     private String email;
-    @Min(6)
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     @NotBlank(message = "Password must be not null")
     private String password;
-    @Min(2)
-    @Max(20)
+    @Size(min = 2, message = "First must be at least 6 characters long and maximum 20 characters.")
     @NotBlank(message = "Firstname must be not null")
     private String firstname;
     @NotBlank(message = "Lastname must be not null")
-    @Min(2)
-    @Max(20)
+    @Size(min = 6, message = "Lastname must be at least 2 characters long and maximum 20 characters.")
     private String lastname;
     @Pattern(regexp = "^\\+380\\d{9}$", message = "Invalid phone number")
     @NotBlank(message = "Phone must be notnull")
