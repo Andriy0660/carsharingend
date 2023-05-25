f7bd1b2ba62be7e419b67f9ddcea794b5ab19f11
Norbert <d4zershow@gmail.com>
2023-05-25 19:04:43 +0300

    Authentication

--- src/main/java/com/example/carsharing/config/SecurityConfiguration.java
+++ src/main/java/com/example/carsharing/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.example.carsharing.filter.JWTAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,9 +30,6 @@ public class SecurityConfiguration {
                 .anyRequest()
                 .authenticated()
                 .and()
-               // .oauth2Login()
-               // .defaultSuccessUrl("/")
-             //   .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -41,9 +37,38 @@ public class SecurityConfiguration {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
-
-
-
-
 }
+//@Configuration
+//@EnableWebSecurity
+//@AllArgsConstructor
+//public class SecurityConfiguration {
+//    private final JWTAuthenticationFilter jwtAuthFilter;
+//    private final AuthenticationProvider authenticationProvider;
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//               // .oauth2Login()
+//               // .defaultSuccessUrl("/")
+//             //   .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//        return http.build();
+//    }
+//
+//
+//
+//
+//
+//}
